ee33ac2b89a2118601594204242beec52afb4d43
Esteban Piazza <estebanpiazza51@gmail.com>
2024-05-27 10:46:11 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// JAVA != JavaScript
+//son cosas distintas
+
+//variables
+//espacio en memoria donde se almacena un valor
+
+let nombre = "Esteban";
+// let - nombre la variable - = - valor - ;
+//si el valor es texto, va entre comillas
+
+let edad = 31 + 1;
+
+console.log("El nombre del profesor es: " + nombre + " y la edad es: " + edad )
+
+// tipos de datos
+// number : 1/2/3/-1/-2/-3/0.1/3.11/0
+// cadenas de caracteres (string): "Esteban", "Hola", "Varela 269", "11"
+// boolean : True y False
+let abierto = false
+let cerrado = true
